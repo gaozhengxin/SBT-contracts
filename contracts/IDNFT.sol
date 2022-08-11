9728c87db20116fa0b0dca1ffade8f4efefacfab
gaozhengxin <zhengxingao@live.cn>
2022-08-11 13:27:48 +0800

    update idcard image

--- contracts/IDNFT.sol
+++ contracts/IDNFT.sol
@@ -5,6 +5,14 @@ import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721Enumer
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts/utils/Base64.sol";
 
+interface IMultiHonor {
+    function POC(uint256 tokenId) view external returns(uint64);
+    function VEPower(uint256 tokenId) view external returns(uint64);
+    function EventPower(uint256 tokenId) view external returns(uint64);
+    function Total(uint256 tokenId) view external returns(uint64); 
+    function Level(uint256 tokenId) view external returns(uint8);
+}
+
 contract IDNFT_v1 is ERC721EnumerableUpgradeable,OwnableUpgradeable {
     function initialize()public initializer{
         __ERC721_init_unchained("IDNFT", "IDNFT");
@@ -12,6 +20,7 @@ contract IDNFT_v1 is ERC721EnumerableUpgradeable,OwnableUpgradeable {
     }
 
     uint256 public tokenId;
+    address public honor;
 
     function _beforeTokenTransfer(
         address from,
@@ -39,12 +48,8 @@ contract IDNFT_v1 is ERC721EnumerableUpgradeable,OwnableUpgradeable {
     }
 
     function _tokenURI(uint _tokenId) internal pure returns (string memory output) {
-        output = '<svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMinYMin meet" viewBox="0 0 350 350"><style>.base { fill: white; font-family: serif; font-size: 14px; }</style><defs><pattern id="img1" x="0" y="0" width="100%" height="100%"><image xlink:href="https://raw.githubusercontent.com/gaozhengxin/assets/main/MarketingNFT.gif" width="45%" height="45%" /></pattern></defs><rect width="100%" height="100%" fill="url(#img1)" /><text x="5" y="150" class="base">';
-        output = string(abi.encodePacked(output, toString(_tokenId)));
-        output = string(abi.encodePacked(output, '</text></svg>'));
-
-        string memory json = Base64.encode(bytes(string(abi.encodePacked('{"name": "lock #', toString(_tokenId), '", "description": "veMULTI NFT", "image": "data:image/svg+xml;base64,', Base64.encode(bytes(output)), '"}'))));
-        output = string(abi.encodePacked('data:application/json;base64,', json));
+        uint lvl = IMultiHonor(honor).Level(_tokenId);
+        output = string(abi.encodePacked('https://gateway.pinata.cloud/ipfs/QmTYwELcSgghx32VMsSGgWFQvCAqZ5tg6kKaPh2MSJfwAj/', toString(lvl)));
     }
 
     function toString(uint256 value) internal pure returns (string memory) {
