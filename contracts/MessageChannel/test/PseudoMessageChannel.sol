d079c7e08277ebc3fea5fe7e5b81db5453288808
gaozhengxin <zhengxingao@live.cn>
2022-10-08 16:33:26 +0800

    update PseudoMessageChannel

--- contracts/MessageChannel/test/PseudoMessageChannel.sol
+++ contracts/MessageChannel/test/PseudoMessageChannel.sol
@@ -3,9 +3,10 @@ pragma solidity ^0.8.0;
 import "../MessageChannelBase.sol";
 
 contract PseudoMessageChannel is MessageChannelBase {
+    event Send(uint256 toChainID, bytes message);
+
     function send(uint256 toChainID, bytes memory message) external override {
-        // Do nothing
-        return;
+        emit Send(toChainID, message);
     }
 
     address public caller = address(0);
