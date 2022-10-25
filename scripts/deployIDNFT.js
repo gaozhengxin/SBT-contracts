b29e3d483f2c814f4d30d12c3f10b070c9b16075
gaozhengxin <zhengxingao@live.cn>
2022-10-25 12:40:52 +0800

    add deploy script

--- scripts/deployIDNFT.js
+++ scripts/deployIDNFT.js
@@ -7,18 +7,12 @@
 const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  const IDNFT_v1 = await hre.ethers.getContractFactory("IDNFT_v1");
+  const idnft = await IDNFT_v1.deploy();
 
-  const lockedAmount = hre.ethers.utils.parseEther("1");
+  await idnft.deployed();
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log("Lock with 1 ETH deployed to:", lock.address);
+  console.log("IDNFT_v1 logic deployed to:", idnft.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
