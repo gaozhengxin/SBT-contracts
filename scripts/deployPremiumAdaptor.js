76c6fcfde702bb3530d1a6d75f282bbd610b91f8
gaozhengxin <zhengxingao@live.cn>
2022-11-02 18:25:18 +0800

    modify deploy scripts

--- scripts/deployPremiumAdaptor.js
+++ scripts/deployPremiumAdaptor.js
@@ -1,9 +1,18 @@
 const hre = require("hardhat");
 
+/*
+// bsc
 let idcard = "0x3E05584358f0Fbfc1909aDE5aCfFBAB7842BdfDc";
 let controller = "0x81DCd47EdAD7e30864C7d3f84032368954889B90";
 let money = "0x8ac76a51cc950d9822d68b83fe1ad97b32cd580d"; // Binance-Peg USDC
 let price = "5000000000000000000" // 5 USDC
+*/
+
+// polygon
+let idcard = "0x7a02492bAa66B0b8266a6d25Bbd6D8BA169296CC";
+let controller = "0xB89a2Fa1efB5BcEcd813319c99711cc15DCa2C00";
+let money = "0x2791bca1f2de4661ed88a30c99a7a9449aa84174"; // Polygon PoS USDC
+let price = "5000000" // 5 USDC
 
 async function main() {
     const [owner] = await ethers.getSigners();
--- scripts/deployV2.js
+++ scripts/deployV2.js
@@ -5,31 +5,44 @@
 // will compile your contracts, add the Hardhat Runtime Environment's members to the
 // global scope, and execute the script.
 const hre = require("hardhat");
+const { expect } = require("chai");
 
 const proxyAdminAddrs = {
-  "polygon": "0xe7E22Ad06493b97dd86875C7F59f0d71C664c75E"
+  "polygon": "0xe7e22ad06493b97dd86875c7f59f0d71c664c75e"
+}
+
+const idnftproxy = {
+  "polygon": "0x7a02492bAa66B0b8266a6d25Bbd6D8BA169296CC"
 }
 
 async function main() {
   const [owner] = await ethers.getSigners();
   console.log("owner " + owner.address);
-
   let proxyAdmin = await ethers.getContractAt("ProxyAdmin", proxyAdminAddrs[hre.network.name]);
   console.log("proxyAdmin " + proxyAdmin.address);
 
   console.log("\ndeploy idnft");
   let IDNFT_Logic = await ethers.getContractFactory("IDCard_V2");
   let idnft_logic = await IDNFT_Logic.deploy();
+  await idnft_logic.deployed();
   console.log("idnft logic " + idnft_logic.address);
 
-  // TODO update idnft
+  let tx = await proxyAdmin.upgrade(idnftproxy[hre.network.name], idnft_logic.address);
+  await tx.wait();
+
+  let idnft = await ethers.getContractAt("IDCard_V2", idnftproxy[hre.network.name].address);
+  await idnft.initV2();
 
   console.log("\ndeploy controller");
   let Controller_Logic = await ethers.getContractFactory("IDCard_V2_Controller");
   let controller_logic = await Controller_Logic.deploy();
   console.log("controller logic " + controller_logic.address);
 
-  let controller_proxy = await Proxy.deploy(controller_logic.address, proxyAdmin.address, initdata);
+  let i_init = new ethers.utils.Interface(["function initialize()"]);
+  let controllerinitdata = i_init.encodeFunctionData("initialize");
+  let controller_proxy = await Proxy.deploy(controller_logic.address, proxyAdmin.address, controllerinitdata);
+  const Proxy = await ethers.getContractFactory("contracts/proxy/TransparentUpgradeableProxy.sol:TransparentUpgradeableProxy");
+  await controller_proxy.deployed();
   console.log("controller proxy " + controller_proxy.address);
 
   let controller = await ethers.getContractAt("IDCard_V2_Controller", controller_proxy.address);
@@ -39,14 +52,15 @@ async function main() {
   // TODO init idnft
 
   console.log("\ngrant controller admin role");
-  await controller.grantRole(role_admin, owner.address);
-  expect(await idnft.hasRole(role_admin, owner.address)).to.equal(true);
+  let role_admin = await controller.DEFAULT_ADMIN_ROLE();
+  expect(await controller.hasRole(role_admin, owner.address)).to.equal(true);
 
   console.log("\ndeploy SBT");
   let MultiHonor_Multichain = "contracts/SBT/" + hre.network.name + "/MultiHonor.sol:MultiHonor_Multichain";
   let SBT_Logic = await ethers.getContractFactory(MultiHonor_Multichain);
   let sbt_logic = await SBT_Logic.deploy();
   console.log("sbt logic " + sbt_logic.address);
+  await sbt_logic.deployed();
 
   // TODO update SBT
 }
